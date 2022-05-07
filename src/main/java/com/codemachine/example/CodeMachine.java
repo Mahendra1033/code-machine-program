9e0b20d6ecfd59e4e08543b9f549a32fbbeefa04
Mahendra1033 <mahendramahiee1033@gmail.com>
2022-05-07 21:42:05 +0530

    Added The Code Machine Project

---
+++ src/main/java/com/codemachine/example/CodeMachine.java
@@ -0,0 +1,13 @@
+package com.codemachine.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class CodeMachine
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
